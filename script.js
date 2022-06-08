0c0d4662a1baa4b1510eebd87bb072ef77f289a2
Arthur <a-murk@mail.ee>
2022-06-08 21:30:55 +0300

    Create addConsonant and addVowel functions
    Functions that add a random consonant or vowel to the lettersChoice array

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "y", "z"];
 const vowels = ["a", "e", "i", "o", "u"];
+const lettersChoice = [];
+
+function addConsonant() {
+    let randomNumber = Math.floor(Math.random() * consonants.length);
+    lettersChoice.push(consonants[randomNumber]);
+}
+
+function addVowel() {
+    let randomNumber = Math.floor(Math.random() * vowels.length);
+    lettersChoice.push(vowels[randomNumber]);
+}
