bf2da4bda4bf46cb51087931c7e1156b14a5f3e2
Arthur <a-murk@mail.ee>
2022-06-08 21:40:15 +0300

    Add event listeners to buttons

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "y", "z"];
 const vowels = ["a", "e", "i", "o", "u"];
 const lettersChoice = [];
+const consonant = document.querySelector('.consonant');
+const vowel = document.querySelector('.vowel');
 
 function addConsonant() {
     let randomNumber = Math.floor(Math.random() * consonants.length);
@@ -11,3 +13,11 @@ function addVowel() {
     let randomNumber = Math.floor(Math.random() * vowels.length);
     lettersChoice.push(vowels[randomNumber]);
 }
+
+consonant.addEventListener('click', () => {
+    addConsonant();
+});
+
+vowel.addEventListener('click', () => {
+    addVowel();
+})
